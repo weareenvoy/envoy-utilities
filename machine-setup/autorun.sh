c807d547a9d25cf924177a96080482f5e904a2af
Alex Franks <alex@weareenvoy.com>
2018-10-03 14:39:32 -0700

    some output to let the user know what's happening

--- machine-setup/autorun.sh
+++ machine-setup/autorun.sh
@@ -12,20 +12,58 @@ CYLANCE_INSTALL_SCRIPT="install.sh"
 
 CCLEANER_DMG="/Volumes/IT/Software/Mac/CCleaner_MacSetup115.dmg"
 
+function sleepdots () {
+	# takes an int, sleeps for that many seconds and prints a dot as progress
+	if [ -z "$1" ]
+	then
+		DOTS=1
+	else
+		DOTS="$1"
+	fi
+
+	while [ "$DOTS" -gt 0 ]
+	do
+		((DOTS--))
+		echo -n "."
+		sleep 1
+	done
+	echo ""
+}
+
 # Start in home dir
 cd
 
+# Let the user know what's happening
+echo "*** Welcome to the ENVOY workstation autoconfig! ***"
+echo
+echo "The first password prompt you see will be a sudo prompt, use this machine's"
+echo "login password."
+echo
+echo "The username/password prompts which follows expect YOUR Okta user credentials."
+echo "Username format is the user portion of your email - everything before the @"
+echo
+echo -n "Thanks and enjoy the ride"
+
+sleepdots 5
+
+echo
+echo "Mounting IT share..."
 # Pull down & run the mounting script
 curl -s $MOUNT_SCRIPT_SRC > $LOCAL_SCRIPT_NAME
 chmod +x $LOCAL_SCRIPT_NAME
 ./$LOCAL_SCRIPT_NAME IT
 
+echo
 # Set up the machine's hostname
 $HOSTNAME_SCRIPT -v
 
+echo 
 # Run scripts in the IT share
 $CYLANCE_SCRIPT_DIR/$CYLANCE_REMOVE_OTHER_AV
 $CYLANCE_SCRIPT_DIR/$CYLANCE_INSTALL_SCRIPT
 
+echo
 # Open CCleaner dmg for copying
 open $CCLEANER_DMG
+
+echo "Done."
