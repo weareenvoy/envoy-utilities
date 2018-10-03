97c4c1c14d21ed644cf3e02ebefecf964d794c21
Alex Franks <alex@weareenvoy.com>
2018-10-03 10:55:21 -0700

    repo init with readme

---
+++ README.md
@@ -0,0 +1,2 @@
+# ENVOY Utilities
+Various scripts which need to be publicly accessible for machine provisioning and initial user setup
---
+++ machine-setup/mountenvoy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# mount the specified share on afp://local.weareenvoy.com interactively
+
+SERVER="afp://local.weareenvoy.com"
+SHARE="$1"
+MOUNTPT="/Volumes/$SHARE"
+
+# first we need to set up the mount point
+if [ -d "$MOUNTPT" ]
+then
+	echo "Mount point $MOUNTPT already exists, checking for mounted file system there..."
+	MOUNTED=`df | egrep -c "$MOUNTPT"`
+	if [[ "$MOUNTED" != 0 ]]
+	then
+		echo "There seems to be a filesystem mounted on $MOUNTPT:"
+		df | egrep -i "$MOUNTPT"
+		echo "Exiting..."
+		exit 1
+fi
+
+sudo mkdir -p 700 $MOUNTPT
+sudo chown envoy:staff $MOUNTPT
+mount_afp -i $SERVER/$SHARE $MOUNTPT
+
+df | egrep "$MOUNTPT"
+echo "Done."
