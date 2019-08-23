de11e979513184eb36113559309ba10d0434b871
Alex Franks <alex@weareenvoy.com>
2019-08-23 12:19:53 -0700

    moving to arrays for the dmg and pkg lists, additional comments, var cleanup

--- machine-setup/autorun.sh
+++ machine-setup/autorun.sh
@@ -31,6 +31,20 @@ SLACK_DMG="${MAC_APP_DIR}/Slack.dmg"
 SONOS_DMG="${MAC_APP_DIR}/Sonos.dmg"
 ADOBECC_DMG="${MAC_APP_DIR}/Adobe/CreativeCloud.dmg"
 
+# Control the order of the mounts - notably AdobeCC should be last as they have 
+# a sneaky pkg in the dmg which just installs the desktop app installer.. D:
+# TODO - pull AdobeCC pkg out of dmg and install in the pkg section
+INSTALL_DMGS=( "${CCLEANER_DMG}" "${ALFRED_DMG}" "${CHROME_DMG}" "${SLACK_DMG}" "${SONOS_DMG}" "${ADOBECC_DMG}" )
+
+# These are symlinked to real pkg files - unclear whether it is desirable or 
+# sane to install large applications (ie Office365) via pkg from the server 
+# directly. Installs over wifi could be subject to inconsistent 
+OFFICE365_PKG="${MAC_APP_DIR}/Office365/Office365.pkg"
+
+# Again, we control the order of the pkg installs because reasons
+INSTALL_PKGS=( "${OFFICE365_PKG}" )
+
+# Utility functions
 function sleepdots () {
 	# takes an int, sleeps for that many seconds and prints a dot as progress
 	if [ -z "$1" ]
@@ -50,34 +64,36 @@ function sleepdots () {
 }
 
 function installdmg () {
-	# takes a path to a dmg file and assumes we can access it
+	# takes a path to a dmg file
 	DMGPATH="$1"
 	if [ ! -e "${DMGPATH}" ]
 	then
 		echo "Can't access ${DMGPATH} or it doesn't exist, skipping..."
 		return
 	fi
-	BASENAME=`basename ${DMGPATH}`
-	APPNAME=`basename ${DMGPATH} | tr '.' ' ' | awk '{print $1}'`
-	read -n 1 -p "Mount ${BASENAME}? [y/n] " INST
+
+	# the DMGPATH is at least a file
+	DMG_FILENAME=`basename ${DMGPATH}`
+	DMG_APPNAME=`basename ${DMGPATH} | tr '.' ' ' | awk '{print $1}'`
+	read -n 1 -p "Mount ${DMG_FILENAME}? [y/n] " INST
 	echo
 	if [ "${INST}" == "y" ]
 	then
 		# Open dmg for copying
 		open $DMGPATH
 		#sleep 1
-		read -n 1 -p "Press a key when done with ${BASENAME}..." OK
+		read -n 1 -p "Press a key after done using ${DMG_FILENAME}..." OK
 		echo
 		# All done here, unmount the dmg
 		# get the mount point we created by opening the dmg
-		#find /Volumes -maxdepth 1 -type d -iname "*${APPNAME}*" -exec umount -v {} \;
-		DEV_ID=`df -l | egrep -i "${APPNAME}" | awk '{print $1}'`
+		#find /Volumes -maxdepth 1 -type d -iname "*${DMG_APPNAME}*" -exec umount -v {} \;
+		DEV_ID=`df -l | egrep -i "${DMG_APPNAME}" | awk '{print $1}'`
 		if [ -z "${DEV_ID}" ]
 		then
 			GUESS_DEV_ID=`df -l | tail -n1 | awk '{print $1}'`
 			GUESS_MNT_NAME=`df -l | tail -n1 | egrep -o '/Volumes.*$'`
-			echo "Didn't find a mounted dmg for ${APPNAME}..."
-			read -n 1 -p "Does ${GUESS_MNT_NAME} look ok? [y/n] " OK
+			echo "Didn't find a mounted dmg for ${DMG_APPNAME}..."
+			read -n 1 -p "Does '${GUESS_MNT_NAME}' look right? [y/n] " OK
 			echo
 			if [ "${OK}" == "y" ]
 			then
@@ -86,6 +102,7 @@ function installdmg () {
 				return
 			fi
 		fi
+		# eject dmg
 		hdiutil detach ${DEV_ID}
 	fi
 }
@@ -129,14 +146,31 @@ $CYLANCE_SCRIPT_DIR/$CYLANCE_REMOVE_OTHER_AV
 $CYLANCE_SCRIPT_DIR/$CYLANCE_INSTALL_SCRIPT
 
 echo
-echo "We'll install some applications via DMG now..."
+echo "We'll install some applications via dmg now..."
 
-installdmg ${CCLEANER_DMG}
-installdmg ${ALFRED_DMG}
-installdmg ${CHROME_DMG}
-installdmg ${SLACK_DMG}
-installdmg ${SONOS_DMG}
-installdmg ${ADOBECC_DMG}
+# loop through the active dmg installers
+for admg in "${INSTALL_DMGS[@]}"
+do
+	installdmg ${admg}
+done
+
+echo
+echo "And we'll install a couple applications via pkg..."
+
+# loop through the active pkg installers
+for apkg in "${INSTALL_PKGS[@]}"
+do
+	PKG_FILENAME=`basename ${apkg}`
+	PKG_APPNAME=`basename ${apkg} | tr '.' ' ' | awk '{print $1}'`
+	echo "Installing ${PKG_FILENAME}..."
+	#sudo installer -pkg ${apkg} -target / -verboseR
+	# we'll do the pkg installs interactively since that's how we're already 
+	# doing the dmg's as well
+	open ${apkg}
+	#sleep 1
+	read -n 1 -p "Press a key when done installing ${PKG_FILENAME}..." OK
+	echo
+done
 
 # Clean up
 echo "Removing temp files..."
